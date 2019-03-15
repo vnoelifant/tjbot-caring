eadd86e95a3da71eb2e52d96acb02ff865225497
vnoelifant <veronicamedrano2019@northwester.u.northwestern.edu>
2019-03-15 17:37:20 -0500

    updating context

--- tj_human_interact.js
+++ tj_human_interact.js
@@ -347,8 +347,10 @@ tj.listen(function(text) {
                 david_response = response.output.text[0];
                 tj.speak(david_response);
                 console.log(tjConfig.robot.name,"says", david_response);
-                context = null
-                console.log(context)
+                //context = null
+                console.log('context',context)
+                var context = {};
+                context.emotion = detectedEmotion.emotion;
               });
             });
           }
