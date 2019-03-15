5d9493260039bc4448002deede2d024cdaaa951b
vnoelifant <veronicamedrano2019@northwester.u.northwestern.edu>
2019-03-15 03:38:51 -0500

    dialogue code updates

--- tj_human_interact.js
+++ tj_human_interact.js
@@ -302,65 +302,60 @@ function emoSadConvo() {
 
 */
 
-function expressEmo() {
-  tj.listen(function(text) {
-    getEmotion(text).then((detectedEmotion) => {
-      var context = {};
-      context.emotion = detectedEmotion.emotion;
-      console.log('context.emotion',context.emotion);
-      tj.wave(); // David indicates he heard you through arm wave
-      //context.emotion = emotion;
 
-      //tj.converse(WORKSPACEID, text, function(response) {
-        //console.log(response.intents[0].intent)
-      assistant.message(
-      {
-        workspace_id: WORKSPACEID,
-        input: {'text': text},
-        context: context
-      },
-        function(err, response) {
-          if (err) {
-            console.log('error:', err);
-          }
-          else {
-            //console.log(JSON.stringify(response, null, 2));
-            //if(response.intents.length > 0 && response.intents[0].intent === "receive-support") {
-            context = response.context;
-            console.log(context);
-            console.log(JSON.stringify(response, null, 2));
-            david_response = response.output.text[0];
-            tj.stopListening();
-            tj.speak(david_response);
+tj.listen(function(text) {
+  getEmotion(text).then((detectedEmotion) => {
+    var context = {};
+    context.emotion = detectedEmotion.emotion;
+    console.log('context.emotion',context.emotion);
+    tj.wave(); // David indicates he heard you through arm wave
+    //context.emotion = emotion;
+
+    //tj.converse(WORKSPACEID, text, function(response) {
+      //console.log(response.intents[0].intent)
+    assistant.message(
+    {
+      workspace_id: WORKSPACEID,
+      input: {'text': text},
+      context: context
+    },
+      function(err, response) {
+        if (err) {
+          console.log('error:', err);
+        }
+        else {
+          //console.log(JSON.stringify(response, null, 2));
+          //if(response.intents.length > 0 && response.intents[0].intent === "receive-support") {
+          context = response.context;
+          console.log(context);
+          console.log(JSON.stringify(response, null, 2));
+          david_response = response.output.text[0];
+          tj.stopListening();
+          tj.speak(david_response);
 
 
-            console.log(tjConfig.robot.name,"says", david_response);
+          console.log(tjConfig.robot.name,"says", david_response);
 
-            if (context.emotion === "sadness"){
-              //tj.resumeListening();
-              tj.listen(function(text) {
-                assistant.message({
-                  workspace_id: WORKSPACEID,
-                  input: {'text': text},
-                  context: context
-                }, (err, response) => {
-                  context = response.context;
-                  console.log(JSON.stringify(response, null, 2));
-                  david_response = response.output.text[0];
-                  tj.speak(david_response);
-                  console.log(tjConfig.robot.name,"says", david_response);
-                });
+          if (context.emotion === "sadness"){
+            //tj.resumeListening();
+            tj.listen(function(text) {
+              assistant.message({
+                workspace_id: WORKSPACEID,
+                input: {'text': text},
+                context: context
+              }, (err, response) => {
+                context = response.context;
+                console.log(JSON.stringify(response, null, 2));
+                david_response = response.output.text[0];
+                tj.speak(david_response);
+                console.log(tjConfig.robot.name,"says", david_response);
               });
-            }
-
-            else {
-              expressEmo();
-            }
+            });
           }
-      });
+        }
     });
   });
-}
+});
 
-expressEmo();
+
 
